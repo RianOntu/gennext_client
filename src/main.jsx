f5536cd888b1caaeeb2ab4734a17dffe1d28f566
RianOntu <rianontu13@gmail.com>
2023-07-26 20:15:15 +0600

    created registration and login system

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,9 @@ import {
 } from "react-router-dom";
 import DashboardLayout from './components/Dashboard/DashboardLayout';
 import DashboardHome from './components/Dashboard/DashboardHome';
+import AuthenticationProvider from './components/Providers/AuthenticationProvider';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
 
 const router = createBrowserRouter([
   {
@@ -16,12 +19,26 @@ const router = createBrowserRouter([
         path:'/',
         element:<DashboardHome></DashboardHome>
       }
+     
     ]
   },
+  {
+    
+      path:'/register',
+      element:<Register></Register>
+    
+  },
+  {
+    path:'/login',
+    element:<Login></Login>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthenticationProvider>
     <RouterProvider router={router} />
+    </AuthenticationProvider>
+    
   </React.StrictMode>,
 )
