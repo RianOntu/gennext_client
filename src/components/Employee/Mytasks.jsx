4a45a67f4f8d00e686a6875ade6833a10fb4507d
RianOntu <rianontu13@gmail.com>
2023-07-27 06:02:00 +0600

    employees can update their work status

--- src/components/Employee/Mytasks.jsx
+++ src/components/Employee/Mytasks.jsx
@@ -1,17 +1,39 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthenticationContext } from '../Providers/AuthenticationProvider';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Mytasks = () => {
+    const notify = () => toast("Work Status Updated!");
     const {user}=useContext(AuthenticationContext);
     const [mytasks,setMytasks]=useState([]);
+    const [selectedValue, setSelectedValue] = useState('');
+    const handleSelectChange = (event,id) => {
+        const selectedStatus = event.target.value;
+        setSelectedValue(selectedStatus);
+        
+        fetch(`http://localhost:5000/updatestatus/${id}`,{
+            method:'PATCH',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify({status:selectedStatus})
+        }).then(res=>res.json()).then(data=>{
+            if(data.modifiedCount){
+notify()
+            }
+        })
+      };
+      
     useEffect(()=>{
         fetch(`http://localhost:5000/mytasks?email=${user?.email}`).then(res=>res.json()).then(data=>setMytasks(data))
-    },[])
+    },[mytasks])
     return (
         <div>
+            <ToastContainer />
         <h1 className='text-center'>My Tasks</h1>
-        <table className="table mt-5">
+      { mytasks.length>0? <table className="table mt-5">
 <thead>
 <tr>
   <th scope="col">Task Name</th>
@@ -23,18 +45,26 @@ const Mytasks = () => {
 </thead>
 <tbody>
 {
-    mytasks.map(task=><tr>
+   mytasks.map(task=><tr>
         <td>{task.task_name}</td>
           <td>{task.due_date}</td>
           <td>{task.assignee}</td>
-          <td>{task.status}</td>
+          <td>
+          <select className="form-select" aria-label="Default select example" value={selectedValue} onChange={(event)=>handleSelectChange(event,task._id)}>
+  <option disabled value="">Select Work Status</option>
+  <option value="Working">Working</option>
+  <option value="Complete">Complete</option>
+  
+  
+</select>
+            </td>
           <td><Link to={`/taskdetails/${task._id}`}><button className='btn btn-primary'>View Task Details</button></Link></td>
         </tr>)
 }
 
 
 </tbody>
-</table>
+</table>:<p className='text-center text-danger'>No task yet</p>}
     </div>
     );
 };
